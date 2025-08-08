4e9dfe00b66efe33aad91771c1346c4c253bb124
Gunish Sharma <gunishsharma20@gmail.com>
2025-08-08 15:51:18 -0600

    Initial Commit

---
+++ Odoro/OdoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdoroApp.swift
+//  Odoro
+//
+//  Created by Gunish Sharma on 2025-08-08.
+//
+
+import SwiftUI
+
+@main
+struct OdoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
